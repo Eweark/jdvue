c443909a80252a5848547ad8fcb97daa75466f70
“Eweark” <727954956@qq.com>
2022-08-08 22:12:37 +0800

    整合

--- src/main/java/com/cykj/controller/GoodsController.java
+++ src/main/java/com/cykj/controller/GoodsController.java
@@ -1,11 +1,6 @@
 package com.cykj.controller;
 
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GoodsController {
-=======
 import com.cykj.bean.TbGoods;
 import com.cykj.service.TbGoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * ProjectName: jdvue
- * Package:  com.cykj.controller
- * ClassName:   GoodsController
- * Author:   Mr.lm
- * Description:  description
- * Date:   2022/8/8  17:57
- * Version:   1.0
- */
 @RestController
 @RequestMapping("/admin")
 public class GoodsController {
@@ -38,12 +24,12 @@ public class GoodsController {
     public String addGoodsPage(){
         return "addGood ";
     }
+
     @RequestMapping("/addGoods")
-    @ResponseBody
     public int addGoods(TbGoods tbGoods){
         int re=tbGoodsService.addGoods(tbGoods);
         return  re;
     }
 
->>>>>>> origin/master
+
 }
